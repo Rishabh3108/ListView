9761d5633f8321c79fc88c22cc4bbe00af8a2186
Rishabh3108 <riahabh.choukse@gmail.com>
2022-06-08 17:26:53 +0530

    add to cart button and buy now button

--- lib/homepage/countercode.dart
+++ lib/homepage/countercode.dart
@@ -12,31 +12,31 @@ class Code extends GetxController {
       "Title": "Teen Titans",
       "Type": "Movie1",
       "Poster":
-          "https://i0.wp.com/keeping-it-reel.com/wp-content/uploads/2011/06/green_lantern_ver14.jpg"
+          "https://i0.wp.com/www.lightscamera-bake.com/wp-content/uploads/2017/06/piratesofthecaribbean_header_v5_6489f07c.jpeg"
     },
     {
       "Title": "Teen Titans",
       "Type": "Movie2",
       "Poster":
-      "https://i0.wp.com/keeping-it-reel.com/wp-content/uploads/2011/06/green_lantern_ver14.jpg"
+      "https://i0.wp.com/www.lightscamera-bake.com/wp-content/uploads/2017/06/piratesofthecaribbean_header_v5_6489f07c.jpeg"
     },
     {
       "Title": "Teen Titans",
       "Type": "Movie3",
       "Poster":
-      "https://i0.wp.com/keeping-it-reel.com/wp-content/uploads/2011/06/green_lantern_ver14.jpg"
+      "https://i0.wp.com/www.lightscamera-bake.com/wp-content/uploads/2017/06/piratesofthecaribbean_header_v5_6489f07c.jpeg"
     },
     {
       "Title": "Teen Titans",
       "Type": "Movie4",
       "Poster":
-      "https://i0.wp.com/keeping-it-reel.com/wp-content/uploads/2011/06/green_lantern_ver14.jpg"
+      "https://i0.wp.com/www.lightscamera-bake.com/wp-content/uploads/2017/06/piratesofthecaribbean_header_v5_6489f07c.jpeg"
     },
     {
       "Title": "Teen Titans",
       "Type": "Movie5",
       "Poster":
-      "https://i0.wp.com/keeping-it-reel.com/wp-content/uploads/2011/06/green_lantern_ver14.jpg"
+      "https://i0.wp.com/www.lightscamera-bake.com/wp-content/uploads/2017/06/piratesofthecaribbean_header_v5_6489f07c.jpeg"
     }
   ];
 }
--- lib/homepage/next page.dart
+++ lib/homepage/next page.dart
@@ -1,9 +1,8 @@
 import 'package:demo/homepage/movie_card.dart';
 import 'package:flutter/material.dart';
-
+import 'package:get/get.dart';
 
 class SecondPage extends StatelessWidget {
-
   SecondPage(
       {required this.imageUrl, required this.title, required this.description});
 
@@ -18,10 +17,35 @@ class SecondPage extends StatelessWidget {
           MovieCard(
             cardHight: 500,
             cardWidth: MediaQuery.of(context).size.width,
-            imageUrl:imageUrl,
-            title:title,
-            description:description,
-          )
+            imageUrl: imageUrl,
+            title: title,
+            description: description,
+          ),
+          Spacer(),
+          GestureDetector(
+            onTap: () {
+              Get.snackbar('Cart', 'Successfully added',backgroundColor: Colors.white);
+            },
+            child: Container(
+              height: 50,
+              width: 300,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [Icon(Icons.add), Text('Add to cart')],
+              ),
+              color: Colors.green[400],
+            ),
+          ),
+          GestureDetector(onTap: (){Get.snackbar('Buy', 'Successfully Buy',backgroundColor: Colors.white);
+          },
+            child: Container(margin: EdgeInsets.only(top: 10,bottom: 20),
+              height: 50,
+              width: 300,
+              color: Colors.blue[400],
+              child: (Text('Buy now')),
+              alignment: Alignment.center,
+            ),
+          ),
         ],
       ),
     );
